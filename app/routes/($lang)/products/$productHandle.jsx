02acdcc0a822cfea1d7cba025aa9b93a2e18b32a
enxiu <hd1783260996@163.com>
2023-08-09 10:42:14 +0800

    增加回到主页aq

--- app/routes/($lang)/products/$productHandle.jsx
+++ app/routes/($lang)/products/$productHandle.jsx
@@ -557,8 +557,8 @@ export default function Product() {
             </div>
 
             <img className='logo' src={`https://platform.antdiy.vip/static/image/dazzlinn_icon.svg`} />
-            {/* <p onClick={() => { window.open('https://' + getShopAddress()) }}><img src="https://platform.antdiy.vip/static/image/hultoo_home.svg" /></p> */}
-            <p></p>
+            <p onClick={() => { window.open('https://' + getShopAddress()) }}><img src="https://platform.antdiy.vip/static/image/hultoo_home.svg" /></p>
+            {/* <p></p> */}
           </div>
         </div>
         <div className="product_details items-start md:gap-6 md:grid-cols-2">
