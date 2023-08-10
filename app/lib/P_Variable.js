a3293a6a05268891329390c30c61fb5eabf9d00a
enxiu <hd1783260996@163.com>
2023-08-10 15:12:05 +0800

    自动获取WhatsApp号码

--- app/lib/P_Variable.js
+++ app/lib/P_Variable.js
@@ -30,8 +30,8 @@ export function getDomain() {
 
 export function openWhatsApp() {
   const whatsApp = {
-    phone: '8615158720588',
-    isOpen: false,
+    // phone: '8615158720588',
+    isOpen: true,
   }
   return whatsApp;
 }
