b2718971bd426394eac51d61b55372dea1c5c560
EAST <hd1783260996@163.com>
2023-11-09 16:07:21 +0800

    恢复WhatsApp联系

--- app/lib/P_Variable.js
+++ app/lib/P_Variable.js
@@ -29,7 +29,7 @@ export function getDomain() {
 export function openWhatsApp() {
   const whatsApp = {
     // phone: '8615158720588',
-    isOpen: false,
+    isOpen: true,
   }
   return whatsApp;
 }
